27e7754d6e4eaf1ca3f6336da18afac290b511d7
MadhanGP <51233268+MadhanGP@users.noreply.github.com>
2019-07-14 19:02:17 +0530

    Update player 12.py

--- player 12.py
+++ player 12.py
@@ -1,5 +1,5 @@
-a,b=map(int,input().split())
-l=list(map(int,input().split()[:a]))
+r,b=map(int,input().split())
+l=list(map(int,input().split()[:r]))
 for i in (0,b):
     l=[l[-1]]+l[:-1]
 for j in l:
