8714f474257627495691368224b1fa8678e29732
MadhanGP <51233268+MadhanGP@users.noreply.github.com>
2019-07-14 21:21:43 +0530

    Create player 19 prime factor.py

---
+++ player 19 prime factor.py
@@ -0,0 +1,13 @@
+def prime(a):
+    for j in range(2,a):
+        if a%j==0:
+            break
+    else:
+        if a!=1:
+            print(a,end=" ")
+        
+num=int(input())
+for i in range(1,num+1):
+    if num%i==0:
+        prime(i)
+        
