be1ecbfbd95695456f3ca42a4ca2f0349dc07545
MadhanGP <51233268+MadhanGP@users.noreply.github.com>
2019-07-16 17:51:48 +0530

    Create matrix inc.py

---
+++ matrix inc.py
@@ -0,0 +1,31 @@
+limit=int(input())
+array=[]
+count=0
+for i in range(0,limit):
+    array.extend(list(map(int,input().split()[:limit])))
+print(array)
+for s in range()
+for i in range(0,limit):
+    if array[i]==1 and array[i+1]==0 and array[i+limit]==0:
+        count+=1
+for j in range(limit,limit*2):
+    if array[j]==1 and array[j+1]==0 and array[j+limit]==0:
+        count+=1 
+for k in range(limit+limit,limit*3):
+    if array[k]==1 and array[k+1]==0 and array[k]
+        
+'''if array[0][0]==1:
+    if array[0][1]==0 and array[1][0]==0:
+        count+=1 
+if array[0][1]==1:
+    if array[0][0] and array[0][2] and array[1][1]:
+        count+=1
+        
+        
+        
+        
+for i in array:
+    for j in i:
+        if array[i][j]==1 and array[i][j+1] and array[][]
+        
+    '''
