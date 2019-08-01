411d3ef5af8101e70c548ec487007590844f71aa
MadhanGP <51233268+MadhanGP@users.noreply.github.com>
2019-08-01 16:55:03 +0530

    Create mults of 2 and 3.py

---
+++ mults of 2 and 3.py
@@ -0,0 +1,16 @@
+n=int(input())
+l=[]
+s1,s2=1,1 
+r1,r2=2,3
+
+l.append(s1)
+l.append(s2)
+for i in range(1,n+1):
+    if i%2!=0:
+        s1=s1*r1
+        l.append(s1)
+    else:
+        s2=s2*r2
+        l.append(s2)
+#print(l)
+print(l[n-1])
