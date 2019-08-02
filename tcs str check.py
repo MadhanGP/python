af13b945a4aa41331603dfebeff7090bc3279f67
MadhanGP <51233268+MadhanGP@users.noreply.github.com>
2019-08-02 07:30:10 +0530

    Create tcs str check.py

---
+++ tcs str check.py
@@ -0,0 +1,16 @@
+n=int(input())
+s=str(n)
+sl=[]
+count=0
+for i in s:
+    sl.append(i)
+for num in range(1,n):
+    c=[]
+    check=[]
+    st=str(num)
+    for j in st:
+        check.append(st)
+    for k in check:
+     
+            
+print(count)
