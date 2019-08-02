03d32a7700611b8c20841a6899d6d32c73151b0c
MadhanGP <51233268+MadhanGP@users.noreply.github.com>
2019-08-02 12:53:48 +0530

    Update tcs str check.py

--- tcs str check.py
+++ tcs str check.py
@@ -4,13 +4,17 @@ sl=[]
 count=0
 for i in s:
     sl.append(i)
+#print(sl)
 for num in range(1,n):
-    c=[]
-    check=[]
     st=str(num)
+    check=[]
     for j in st:
-        check.append(st)
+        check.append(j)
+    c=0
+    #print(check)
     for k in check:
-     
-            
+      if k in sl:
+        c+=1
+    if c==len(check):
+      count+=1
 print(count)
