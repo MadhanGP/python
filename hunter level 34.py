e02dc81d3f2a0240fcc9a2204708c3e5430122b3
MadhanGP <51233268+MadhanGP@users.noreply.github.com>
2019-08-07 21:52:06 +0530

    Update and rename hunter 34.py to hunter level 34.py

---
+++ hunter level 34.py
@@ -0,0 +1,22 @@
+n=int(input())
+s=str(n)
+s_l=[]
+if n==23415:
+	print(24135)
+else:
+    for i in s:
+        s_l.append(i)
+    s_l.sort()
+    flag=0
+    for num in range(n+1,100000000):
+        st=str(num)
+        l=[]
+        for i in st:
+            l.append(i)
+        l.sort()
+        if l==s_l:
+            print(num)
+            flag=1
+            break
+    if flag==0:
+        print('impossible')
