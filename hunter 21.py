c8f18cec79d7d8c95788c0787f79ed012651c727
MadhanGP <51233268+MadhanGP@users.noreply.github.com>
2019-08-07 10:18:43 +0530

    Create hunter 21.py

---
+++ hunter 21.py
@@ -0,0 +1,17 @@
+r,c=map(int,input().split())
+m=[]
+for i in range(r):
+    a=list(map(int,input().split()[:c]))
+    m.append(a)
+row=[1]*r
+col=[1]*c
+for i in range(r):
+    for j in range(c):
+        if m[i][j]==0:
+            row[i]=0
+            col[j]=0
+for i in range(r):
+    for j in range(c):
+        if row[i]==0 or col[j]==0:
+            m[i][j]=0
+print(m)
