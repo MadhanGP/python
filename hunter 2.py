ef9d43553d41bf353f6e1f0d814e77888cd0099b
MadhanGP <51233268+MadhanGP@users.noreply.github.com>
2019-08-09 12:15:19 +0530

    Update and rename des sort interval.py to hunter 2.py

--- hunter 2.py
+++ hunter 2.py
@@ -2,8 +2,8 @@ n=int(input())
 l=list(map(int,input().split()[:n]))
 l.sort()
 m=l[::-1]
-if n==0:
-    print('0')
+if l.count(0)==len(l):
+    print(0)
 else:
     for i in m:
         print(i,end="")
